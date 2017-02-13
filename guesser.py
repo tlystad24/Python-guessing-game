6f2eec536575c29ece68e4c5840653800bd54e10
Tobias Lystad <tobias24@me.com>
2017-02-13 22:26:09 +0100

    Update guesser.py

--- guesser.py
+++ guesser.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-"""guesser.py, by Tobias Lystad
+"""Python guessing game by @tlystad24
 This program creates a random number between 1 and 100 and has the user guess it.
 """
 import random
 
 def main():
 	print "Guess a number netween 1 and 100."
-	#randomNumber = 35
+	#randomNumber = 35 <- Number used for debugging and early development.
 	randomNumber = random.randint(1,100)
-	found = False # flag variable to see if they guessed it
+	found = False # flag variable to see if they guessed it.
 
 	while not found:
 	
